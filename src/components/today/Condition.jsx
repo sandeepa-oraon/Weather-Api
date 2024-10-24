531e3034cb7a16d89405a0f2a816f37378141edb
Sandy2004 <134693152+sandeepa-oraon@users.noreply.github.com>
2024-10-24 11:44:28 +0530

    Update Condition.jsx

--- src/components/today/Condition.jsx
+++ src/components/today/Condition.jsx
@@ -1,1 +1,11 @@
-
+// function Condition({forecastData}){
+//     return(
+//         <>
+//             <div className="box condition">
+//                 <h4>{forecastData.current.condition.text}</h4>
+//                 <img className="" src={forecastData.current.condition.icon} />
+//             </div>
+//         </>
+//     )
+// }
+// export default Condition
